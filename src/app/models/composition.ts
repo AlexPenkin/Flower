c011dbd6c6447f7433bb19bb18cafda73a5f35b4
AlexPenkin <sanekpenkin@gmail.com>
2017-11-14 12:00:01 +0300

    all elements (as far) in composition constructor

--- src/app/models/composition.ts
+++ src/app/models/composition.ts
@@ -1,20 +1,51 @@
-import { Element, N, P, K } from './Elements';
+import { Element, N, P, K, Ca, Mg, Fe, S, B, Co, Cu, Mn, Mo,  Zn } from './Elements';
 
 export interface IComposition {
-    N: Element;
-    P: Element;
-    K: Element;
+    N?: Element;
+    P?: Element;
+    K?: Element;
+    Ca?: Element;
+    Mg?: Element;
+    S?: Element;
+    Fe?: Element;
+    B?: Element;
+    Co?: Element;
+    Cu?: Element;
+    Mn?: Element;
+    Mo?: Element;
+    Zn?: Element;
 }
 
 export class Composition implements IComposition {
     N = new N(0);
     P = new P(0);
     K = new K(0);
-    constructor(composition?: { N?, P?, K?}) {
+    Ca = new Ca(0);
+    Mg = new Mg(0);
+    S = new S(0);
+    Fe = new Fe(0);
+    B = new B(0);
+    Co = new Co(0);
+    Cu = new Cu(0);
+    Mn = new Mn(0);
+    Mo = new Mo(0);
+    Zn = new Zn(0);
+    constructor(composition?: IComposition) {
         if (composition) {
-            this.N = composition.N;
-            this.P = composition.P;
-            this.K = composition.K;
+            for (const key in composition) {
+                if (composition[key]) {
+                    this[key] = composition[key];
+                }
+
+            }
+            // this.N = composition.N;
+            // this.P = composition.P;
+            // this.Ca = composition.Ca;
+            // this.Mg = composition.Mg;
+            // this.S = composition.S;
+            // this.Fe = composition.Fe;
+            // this.K = composition.K;
+            // this.K = composition.K;
         }
     }
 }
